6ca5c63a82a36784c69687dc7fc2b5686d15aa5b
Omari Archer <omari.archer21@gmail.com>
2022-03-24 20:40:11 -0400

    API added for GET player function

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+// $.ajax("https://api-nba-v1.p.rapidapi.com/players?id=1", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "7c7529e2ddmsh83ce8c3aeea5151p1c934ejsn5b367de211ba"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.log(err);
+// });
