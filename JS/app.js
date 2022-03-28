c1c2fd99933a288f2856dcd08613f2105b949840
Omari Archer <omari.archer21@gmail.com>
2022-03-28 17:51:08 -0400

    recent changes

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,121 @@
-// $.ajax("https://api-nba-v1.p.rapidapi.com/players?id=1", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "7c7529e2ddmsh83ce8c3aeea5151p1c934ejsn5b367de211ba"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
+//Set Variablaes 
+const $input =$("input")
+const $button =$("button")
+const $aside =$("aside")
+
+
+
+//Create a function to get value from input( this goes somewhere not sure where)
+
+
+//Make API Call 
+
+$button.on("click", () => {
+
+
+
+	const searchPlayer =$input.val()
+
+	const data = {
+		"url": `https://free-nba.p.rapidapi.com/players?&search=${searchPlayer}`,
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+			"X-RapidAPI-Key": "2cc20d9a77msh1f6709770f552d4p19081ajsn96b3210eed57"
+		}
+	};
+	
+	$.ajax(data).done(function (response) {
+		console.log(response);
+//Player photo API blocked by cors
+	// $.ajax(`https://api.sportradar.us/nba-images-t3/getty//schema/manifest-v2.5.xsd?api_key=g7zvf7ufe7mfx237ws6gxzdd
+
+	// `).then((player) => {
+
+	// console.log(player)
+
+$($aside).html(`
+<h4 First Name>First Name: ${response.data[0].first_name}</h4>
+<h4 Last Name>Last Name: ${response.data[0].last_name}</h4>
+<h4 Position>Position: ${response.data[0].position}</h4>
+<h4 Feet>Ft: ${response.data[0].height_feet}</h4> 
+<h4 Inches>In: ${response.data[0].height_inches}</h4>
+<h4 Weight>Weight: ${response.data[0].weight_pounds}</h4>
+<h4 Team>Team: ${response.data[0].team.city}</h4>
+<h4 Team>: ${response.data[0].team.name}</h4>
+<h4 Team>: ${response.data[0].team.abbreviation}</h4>
+
+
+`)
+$inputValue=""
+})
+	});
+	$button.on("click", () => {
+
+	const teamData = {
+		"url": "https://free-nba.p.rapidapi.com/teams?page=0",
+		"method": "GET",
+		"headers": {
+			"X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+			"X-RapidAPI-Key": "2cc20d9a77msh1f6709770f552d4p19081ajsn96b3210eed57"
+		}
+	};
+	
+	$.ajax(teamData).done(function (response) {
+		console.log(response);
+	});
+
+})
+
+//Code Grave Yard	
+// After data is called
+// .then((data) => {
+// 	console.log(data)
+    // let playerID =${data.id}
+
+// console.log(data.data[0].first_name)
+// console.log(data.data[0].last_name)
+// console.log(data.meta.total_count)
+    //Loop through the Array for the data
+
+//     data.forEach((first_name)=>{
+//         for (let first_name in data){
+//         console.log(first_name)
+
+//         }
+
+//     $("aside").html(`
+// <h1>${data.first_name}</h1>`)
+
+   
 // })
 // .catch(err => {
 // 	console.log(err);
-// });
+// })
+
+// After data is called
+// .then((data) => {
+// 	console.log(data)
+    // let playerID =${data.id}
+
+// console.log(data.data[0].first_name)
+// console.log(data.data[0].last_name)
+// console.log(data.meta.total_count)
+    //Loop through the Array for the data
+
+//     data.forEach((first_name)=>{
+//         for (let first_name in data){
+//         console.log(first_name)
+
+//         }
+
+//     $("aside").html(`
+// <h1>${data.first_name}</h1>`)
+
+   
+// })
+// .catch(err => {
+// 	console.log(err);
+// })
+// })
+
--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 
 ** The NBA Profile App lets the user select an active NBA player and Team and reveal stats about the search. **
+
+** Tech Used**
+HTML5 
+JAVA SCRIPT
+CCS
+JQUERY
+AJAX
+GOOGLE FONTS
+FREE NBA API
+*** APP DISPLAY **
